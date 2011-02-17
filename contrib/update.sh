2e126c66b9a78713a3600ed2d8bec07b0ff5f477
Michael Davidsaver <mdavidsaver@bnl.gov>
2011-02-17 11:26:03 -0500

    update.sh automatically run as correct user

--- contrib/update.sh
+++ contrib/update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "`whoami`" != channelarchiver ]; then
+    sudo -u channelarchiver $0 "$@"
+    exit $?
+fi
+
 /usr/share/channelarchiver/updateindex.sh
 
 if [ -x /usr/share/channelarchiver/genserver.sh ]
